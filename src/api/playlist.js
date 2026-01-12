8a3bc8feceecda6bb23fb036bcadce8bb6736375
macabc <anej.batagelj@gmail.com>
2026-01-12 16:30:45 +0100

    test

--- src/api/playlist.js
+++ src/api/playlist.js
@@ -3,17 +3,17 @@ import { API_URLS } from '@/utils/constants'
 
 export const playlistAPI = {
   async getAllPlaylists() {
-    const response = await client.get(`${API_URLS.PLAYLIST}/playlist`)
+    const response = await client.get(`${API_URLS.PLAYLIST}`)
     return response.data
   },
 
   async getPlaylist(playlistId) {
-    const response = await client.get(`${API_URLS.PLAYLIST}/playlist/${playlistId}`)
+    const response = await client.get(`${API_URLS.PLAYLIST}/${playlistId}`)
     return response.data
   },
 
   async createPlaylist(name, description = null) {
-    const response = await client.post(`${API_URLS.PLAYLIST}/playlist`, {
+    const response = await client.post(`${API_URLS.PLAYLIST}`, {
       name,
       description,
     })
@@ -21,15 +21,15 @@ export const playlistAPI = {
   },
 
   async deletePlaylist(playlistId) {
-    await client.delete(`${API_URLS.PLAYLIST}/playlist/${playlistId}`)
+    await client.delete(`${API_URLS.PLAYLIST}/${playlistId}`)
   },
 
   async addSongToPlaylist(playlistId, songId) {
-    await client.post(`${API_URLS.PLAYLIST}/playlist/${playlistId}/song/${songId}`)
+    await client.post(`${API_URLS.PLAYLIST}/${playlistId}/song/${songId}`)
   },
 
   async removeSongFromPlaylist(playlistId, songId) {
-    await client.delete(`${API_URLS.PLAYLIST}/playlist/${playlistId}/song/${songId}`)
+    await client.delete(`${API_URLS.PLAYLIST}/${playlistId}/song/${songId}`)
   },
 }
 
